b2012a7481f8d0e844bc068630db6f56527f7c10
SUMIT KUMAR MAITY <sumitkumarmaity2006@gmail.com>
2026-07-19 23:36:29 +0530

    Time: 16 ms (45.11%), Space: 48 MB (74.97%) - LeetHub

---
+++ 0477-total-hamming-distance/0477-total-hamming-distance.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int totalHammingDistance(int[] nums) {
+        int n = nums.length;
+        int total = 0;
+        for (int bit = 0; bit < 32; bit++) {
+            int ones = 0;
+            for (int num : nums) {
+                if (((num >> bit) & 1) == 1) {
+                    ones++;
+                }
+            }
+            int zeros = n - ones;
+            total += ones * zeros;
+        }
+        return total;
+    }
+}
