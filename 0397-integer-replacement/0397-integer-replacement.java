205dee9baf84fe4dbba49c231f401b9b224cfb5c
SUMIT KUMAR MAITY <sumitkumarmaity2006@gmail.com>
2026-07-26 20:26:27 +0530

    Time: 0 ms (100%), Space: 41.7 MB (95.93%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public int integerReplacement(int n) {
+        return helper((long) n);
+    }
+    private int helper(long n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return 1 + helper(n / 2);
+        }
+        if (n == 3 || n % 4 == 1) {
+            return 1 + helper(n - 1);
+        }
+        return 1 + helper(n + 1);
+    }
+}
